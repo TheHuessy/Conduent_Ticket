29e4fb21dc8505af588222cb1cb1d17b79a1c87e
Huessy <152226@cityhall.boston.cob>
2018-12-20 13:44:00 -0500

    Updated workflow to run more smoothly by updating python script and calling it from a command sent within R session

---
+++ Data Consolidation.R
@@ -0,0 +1,136 @@
+library(readr)
+library(dplyr)
+library(XML)
+library(xml2)
+
+##### Load most recent datasets #####
+system("cmd.exe", input ='python "H:/My Documents/Git Code Examples/Conduent_Ticket/piptocsv.py"')
+
+print("Python done doing its magic")
+
+##### Combining Ticket Data #####
+f <- list.files(path = "H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/Comma_Add") %>% 
+  .[grep("ticket_extract",.)]
+
+
+ticq <- data.frame("Agency.Name..Issuing.Agency." = as.character(),
+                  "Ticket.Number" = as.numeric(),
+                  "Ticket.Issue.Date" = as.numeric(),
+                  "Ticket.Proc.Date" = as.character(),
+                  "Batch.Date" = as.character(),
+                  "Batch.Number" = as.numeric(),
+                  "Badge....Issuing.Officer." = as.numeric(),
+                  "Issuing.Officer.Division" = as.numeric(),
+                  "Route" = as.character(),
+                  "Street.No" = as.numeric(),
+                  "Issuing.Officer.Name" = as.character(),
+                  "Street.Name" = as.character(),
+                  "Street.Direction" = as.character(),
+                  "Street.Suffix" = as.character(),
+                  "Amount.Due" = as.numeric(),
+                  "Rp.Name" = as.character(),
+                  "Make" = as.character(),
+                  "Rp.Plate.State" = as.character(),
+                  "Rp.Plate" = as.character(),
+                  "Rp.Plate.Type" = as.character(),
+                  "Plate.Color" = as.character(),
+                  "Vehicle.Color" = as.character(),
+                  "Vehicle.Color.Desc" = as.character(),
+                  "Violation.Code" = as.character(),
+                  "Violation.Type" = as.character(),
+                  "Violation.Type.Desc" = as.character(),
+                  "Violation.Class.Id" = as.character(),
+                  "Violation.Class.Desc" = as.character(),
+                  "Violation.Desc" = as.character(),
+                  "Ticket.Issue.Time" = as.character()
+                  )
+
+
+for (i in 1:length(f)){
+  pt <- paste("H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/Comma_Add/", f[i], sep = "")
+  g <- read.csv(pt, header = TRUE, comment.char = "", stringsAsFactors = FALSE, fill = TRUE)
+  ticq <- rbind(ticq, g)
+  print(paste("Finished with", i, "of", length(f)))
+}
+
+head(ticq)
+
+##### Combining Payment Data #####
+
+pf <- list.files(path = "Z:/XEROX/") %>% 
+  .[grep("payment_extract",.)]
+
+
+payq <- data.frame("Pay.Type" = as.character(),
+                   "Pay.Ticket.Number" = as.numeric(),
+                   "Pay.Accnt.Desc" = as.character(),
+                   "Pay.Method" = as.character(),
+                   "Pay.Proc.Date" = as.character(),
+                   "Pay.Proc.Time" = as.character(),
+                   "Pay.Deposit.Date" = as.character(),
+                   "Pay.Batch.Num" = as.numeric(),
+                   "Pay.Clerk" = as.character(),
+                   "Payment.Amt" = as.numeric())
+
+
+
+
+for (i in 1:length(pf)){
+  pt <- paste("Z:/XEROX/", pf[i], sep = "")
+  g <- read.table(pt, sep = "|", header = TRUE, comment.char = "", stringsAsFactors = FALSE, fill = TRUE)
+  payq <- rbind(payq, g)
+  print(paste("Finished with", i, "of", length(f)))
+}
+
+head(payq)
+
+##### Combining Penalty Data #####
+
+f <- list.files(path = "Z:/XEROX/") %>% 
+  .[grep("penalty_extract",.)]
+
+
+penq <- data.frame("Ticket.Number" = as.numeric(),
+                   "Ticket.Issue.Date" = as.character(),
+                   "Ticket.Proc.Date" = as.character(),
+                   "Amount.Due" = as.numeric(),
+                   "Total.Fine.Amount" = as.numeric(),
+                   "Interest.Due" = as.character(),
+                   "Fine.Amount" = as.numeric(),
+                   "Penalty.1" = as.numeric(),
+                   "Penalty.2" = as.character(),
+                   "Penalty.3" = as.character(),
+                   "Penalty.4" = as.character(),
+                   "Penalty.5" = as.character(),
+                   "Reduction.Amount" = as.numeric(),
+                   "Total.Paid" = as.numeric(),
+                   "Unapplied.Amount" = as.character(),
+                   "Penalty.Date..1st.Occurrence." = as.character(),
+                   "Penalty.Date..2nd.Occurrence." = as.character(),
+                   "Penalty.Date..3rd.Occurrence." = as.character()
+                   )
+
+
+
+
+for (i in 1:length(f)){
+  pt <- paste("Z:/XEROX/", f[i], sep = "")
+  g <- read.table(pt, sep = "|", header = TRUE, comment.char = "", stringsAsFactors = FALSE, fill = TRUE)
+  penq <- rbind(penq, g)
+  print(paste("Finished with", i, "of", length(f)))
+}
+
+head(penq)
+
+#Good until Christmas VV
+Sys.setenv(CIVIS_API_KEY = 'e55b1cc634361b4a71cdd072d0dec1bfb8df0522aba379d7d2a422f124abf8ba')
+
+library(civis)
+
+write_civis(ticq, 'sandbox.conduent_tickets', if_exists = "drop")
+#write.csv(ticq, "H:/My Documents/Git Code Examples/Conduent_Ticket/tickets.csv", row.names = FALSE)
+write_civis(payq, 'sandbox.conduent_payments', if_exists = "drop")
+write_civis(penq, 'sandbox.conduent_penalties', if_exists = "drop")
+
+
+
--- piptocsv.py
+++ piptocsv.py
@@ -6,21 +6,48 @@ import csv
 import os
 
 fnames = []
-
-for dirname, dirnames, filenames in os.walk('H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/'):
+##### CHECKING XEROX REPOSITORY #####
+#for dirname, dirnames, filenames in os.walk('H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/'):
+for dirname, dirnames, filenames in os.walk('Z:/XEROX/'):
 	for filename in filenames:
 		if 'ticket_extract' in filename:
 			#print(os.path.join(dirname,filename))	
 			fnames.append(os.path.join(filename))
 
-os.chdir('H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/')
 
+##### LOOKING AT WHAT HAS ALREADY BEEN TRANSFORMED #####
+cnames = []
+for dirname, dirnames, filenames in os.walk('H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/Comma_Add/'):
+	for filename in filenames:
+		if 'ticket_extract' in filename:
+			#print(os.path.join(dirname,filename))	
+			cnames.append(os.path.join(filename))
+
+##### CREATING LIST OF NEW FILE NAMES #####
+#This is a little redundant but it allows me to produce more informative console outputs
+nnames = []
 for fn in fnames:
-	with open(fn, 'r') as file:
-		filedata = file.read()
-	filedata = filedata.replace(',', '--')
-	filedata = filedata.replace('|', ',')
-	filedata = filedata.replace('\\', '')
-	with open('Comma_Add/'+fn, 'w') as file:
-		file.write(filedata)
-	print("Finished file conversion " + str(fnames.index(fn)+1) + " of " + str(len(fnames)))
+	if fn not in cnames:
+		nnames.append(fn)
+
+##### ITERATING THROUGH THE FILE NAMES THAT AREN'T PRESENT IN TRANSFORMED REPOSITORY #####
+if len(nnames) > 0:
+	for nn in nnames:
+		with open(str('Z:/XEROX/'+nn), 'r') as file:
+			filedata = file.read()
+		filedata = filedata.replace(',', '--')
+		filedata = filedata.replace('|', ',')
+		filedata = filedata.replace('\\', '')
+		with open('H:/My Documents/Git Code Examples/Conduent_Ticket/Data Imports/Comma_Add/'+nn, 'w') as file:
+			file.write(filedata)
+		print("Finished file conversion " + str(nnames.index(nn)+1) + " of " + str(len(nnames)))
+	print("Goodbye")
+else:
+	print("No new datasets!")
+	print("Goodbye")
+
+
+
+
+
+
